5caad5657a32187ab16c9707ca9b84c448fcdf59
YYS <3212592665@qq.com>
2024-02-28 00:49:06 +0800

    研究一下配置文件

--- apps/play.js
+++ apps/play.js
@@ -3,15 +3,46 @@ import { segment } from 'oicq'
 import plugin from "../../../lib/plugins/plugin.js";
 import yaml from 'js-yaml';
 import path from 'path';
-import { readFileSync } from 'fs';
 import { fileURLToPath } from 'url';
+import fs from 'fs/promises';
+import makeConfig from "../../../lib/plugins/config.js"
+import { rulePrefix } from '../models/common.js'
+const { config, configSave } = await makeConfig("bot_id",
+    {
+        permission: "master",
+        bot_id: [],
+    },
+)
+// async function ensureConfigFileAndLoad() {
+//     const __filename = fileURLToPath(import.meta.url);
+//     const configDir = path.resolve(path.dirname(__filename), '..', 'config');
+//     const configPath = path.join(configDir, 'play.yml');
 
-// 加载配置文件
-const __filename = fileURLToPath(import.meta.url);
-const configPath = path.resolve(path.dirname(__filename), '..', 'config', 'play.yml');
-const configData = readFileSync(configPath, 'utf8');
-const config = yaml.load(configData);
+//     try {
+//         // 检查 play.yml 是否存在
+//         await fs.access(configPath, fs.constants.F_OK);
 
+//         // 加载 play.yml 配置文件
+//         const configData = await fs.readFile(configPath, 'utf8');
+//         const config = yaml.load(configData);
+
+//         // 检查bot_id是否配置
+//         if (!config || !config.bot_id) {
+//             console.log('bot_id is not configured in config/play.yml');
+//         }
+
+//         return config;
+//     } catch (error) {
+//         // 如果文件不存在或bot_id未配置，则创建并写入默认配置
+//         if (error.code === 'ENOENT') {
+//             const defaultConfig = { bot_id: '' };
+//             await fs.writeFile(configPath, yaml.dump(defaultConfig), 'utf8');
+//             return defaultConfig;
+//         } else {
+//             logger.error(error);
+//         }
+//     }
+// }
 
 export class PlayGenshinImpact extends plugin {
     // 构造函数
@@ -25,6 +56,10 @@ export class PlayGenshinImpact extends plugin {
                 {
                     reg: /^[#/]?(原神|genshin impact)(,|，)?(启动|start)(!|！)$/,
                     fnc: "play",
+                }, {
+                    reg: /^(test|插件)?设置bot_id:\d+$/,
+                    fnc: "setId",
+                    permission: config.permission
                 },
             ],
         });
@@ -73,4 +108,18 @@ export class PlayGenshinImpact extends plugin {
             return true;
         }
     }
+    async setId() {
+        const botId = this.e.msg.replace(/^(test|插件)?设置bot_id:/, "").trim();
+        if (!botId) {
+            this.reply('无效的输入格式，请按照“设置bot_id<数字>”的格式输入', true);
+            return;
+        }
+        // 覆盖原有配置中的bot_id值
+        config.bot_id = botId;
+        // 保存新的配置
+        await configSave();
+        this.reply(`bot_id已成功设置为${botId}，重启后生效`, true);
+    }
+
 }
+export default PlayGenshinImpact;
